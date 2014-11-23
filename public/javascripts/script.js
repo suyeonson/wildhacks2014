d2f625c9fccdd3c200bf74f4eb79ddc8e6fe0bcf
nicolezhu <nzhu94@gmail.com>
2014-11-23 03:34:39 -0600

    reorder js file

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,42 +2,51 @@ var calendarLink = "https://www.googleapis.com/calendar/v3/calendars/4cpkvhth0nv
 
 
 
-
-$.getJSON(calendarLink, function(data) {
-	
-	var response = data["items"]
-
-	var timeDifferences = [];
-
-	console.log(response)
-
-	for (var i=0; i < response.length-1; i++) {
-
-		if (response[i+1]["end"]["dateTime"].split("T")[0] == response[i]["start"]["dateTime"].split("T")[0]) {
-			timeDifferences.push({
-				"startId": response[i]["id"],
-				"endId": response[i+1]["id"],
-				"timeDifference": moment.utc(moment(response[i+1]["start"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss").diff(moment(response[i]["end"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss"))).format("HH:mm:ss")
-			});
-		}; 
-	};
-
-console.log(timeDifferences)
-});
-
 $(document).ready(function() {
-	var saved_articles = {};
-	saved_articles.list = [];
-	$('.item').each(function(i) {
-		var reading_time = $(this).find('#reading-time').text();
-		var url = $(this).find('a').attr('href');
-		var title = $(this).find('a').text();
-		var new_article = {
-			'title': title,
-			'url': url,
-			'reading_time': reading_time
+	$.getJSON(calendarLink, function(data) {
+		
+		var response = data["items"]
+
+		var timeDifferences = [];
+
+		// console.log(response)
+
+		for (var i=0; i < response.length-1; i++) {
+
+			if (response[i+1]["end"]["dateTime"].split("T")[0] == response[i]["start"]["dateTime"].split("T")[0]) {
+				timeDifferences.push({
+					"startId": response[i]["id"],
+					"endId": response[i+1]["id"],
+					"timeDifference": moment.utc(moment(response[i+1]["start"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss").diff(moment(response[i]["end"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss"))).format("HH:mm:ss")
+				});
+			}; 
 		};
-		saved_articles.list.push(new_article);
+
+		var saved_articles = {};
+		saved_articles.list = [];
+		$('.item').each(function(i) {
+			var reading_time = $(this).find('#reading-time').text();
+			var url = $(this).find('a').attr('href');
+			var title = $(this).find('a').text();
+			var new_article = {
+				'title': title,
+				'url': url,
+				'reading_time': reading_time
+			};
+			saved_articles.list.push(new_article);
+		});
+		
+		// console.log(saved_articles);
+		console.log(timeDifferences);
+
+		var valid_articles = {};
+		valid_articles.list = [];
+		for (var i=0; i < saved_articles.list.length; i++) {
+			var reading_time = saved_articles.list[i].reading_time;
+			for (var j=0; j < timeDifferences.length; j++) {
+				console.log(timeDifferences[j].timeDifference);
+				// if (reading_time < timeDifferences[j].)
+			}
+		}
 	});
-	console.log(saved_articles);
 });
