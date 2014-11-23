9faeb0723ab750193c48ee2eef7f17c493c2c3ac
Suyeon Son <suyeon.son@icloud.com>
2014-11-23 03:50:41 -0600

    turn time difference to minutes

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,29 +1,51 @@
-var calendarLink = "https://www.googleapis.com/calendar/v3/calendars/4cpkvhth0nvtedeo8t7tlfbupk@group.calendar.google.com/events?key=AIzaSyDFIPR7NpYdr5-2ykZqjoMsuT9EYW_zt_M";
+var getCalendar = "https://www.googleapis.com/calendar/v3/calendars/4cpkvhth0nvtedeo8t7tlfbupk@group.calendar.google.com/events?key=AIzaSyDFIPR7NpYdr5-2ykZqjoMsuT9EYW_zt_M";
+
+var postToCalendar = "https://www.googleapis.com/calendar/v3/calendars/4cpkvhth0nvtedeo8t7tlfbupk@group.calendar.google.com/events/quickAdd?key=AIzaSyDFIPR7NpYdr5-2ykZqjoMsuT9EYW_zt_M";
 
 
 
+// ?text={QUICK_ADD_TEXT}&key=AIzaSyDFIPR7NpYdr5-2ykZqjoMsuT9EYW_zt_M
 
-$.getJSON(calendarLink, function(data) {
+$.getJSON(getCalendar, function(data) {
 	
 	var response = data["items"]
 
 	var timeDifferences = [];
 
-	console.log(response)
+	// console.log(response)
 
 	for (var i=0; i < response.length-1; i++) {
 
 		if (response[i+1]["end"]["dateTime"].split("T")[0] == response[i]["start"]["dateTime"].split("T")[0]) {
+
+
+			var startTime = moment(response[i+1]["start"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss")
+
+			var endTime = moment(response[i]["end"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss")
+
 			timeDifferences.push({
 				"startId": response[i]["id"],
 				"endId": response[i+1]["id"],
-				"timeDifference": moment.utc(moment(response[i+1]["start"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss").diff(moment(response[i]["end"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss"))).format("HH:mm:ss")
+				"timeDifference": moment.duration(startTime.diff(endTime)).asMinutes()
 			});
 		}; 
 	};
 
-console.log(timeDifferences)
+	console.log(timeDifferences);
 });
+
+// var params = {
+// 	"text": "thing at 11/23 10 - 10:15 am"
+// };
+
+// xmlhttp = new XMLHttpRequest();
+// xmlhttp.open("POST", postToCalendar, true);
+// xmlhttp.setRequestHeader("Content-type", "text/plain");
+
+
+
+
+// xmlhttp.send("text=thing+at+11%2F23+10+-+10%3A30+am")
 
 $(document).ready(function() {
 	var reading_time = $('#reading-time').val();
