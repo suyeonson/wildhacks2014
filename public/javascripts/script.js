b77176f0ac2ff8016baf0b499b8fad86baac4dcf
Suyeon Son <suyeon.son@icloud.com>
2014-11-23 02:52:25 -0600

    push time differences and correlating events to array

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,25 +1,27 @@
 var calendarLink = "https://www.googleapis.com/calendar/v3/calendars/4cpkvhth0nvtedeo8t7tlfbupk@group.calendar.google.com/events?key=AIzaSyDFIPR7NpYdr5-2ykZqjoMsuT9EYW_zt_M";
 
+
+
+
 $.getJSON(calendarLink, function(data) {
 	
 	var response = data["items"]
+
+	var timeDifferences = [];
 
 	console.log(response)
 
 	for (var i=0; i < response.length-1; i++) {
 
 		if (response[i+1]["end"]["dateTime"].split("T")[0] == response[i]["start"]["dateTime"].split("T")[0]) {
-			console.log("between " + response[i]["id"] + " & " + response[i+1]["id"])
-			console.log("the difference is " + moment.utc(moment(response[i+1]["start"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss").diff(moment(response[i]["end"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss"))).format("HH:mm:ss"))		
-		} 
-
-
-
-
+			timeDifferences.push({
+				"startId": response[i]["id"],
+				"endId": response[i+1]["id"],
+				"timeDifference": moment.utc(moment(response[i+1]["start"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss").diff(moment(response[i]["end"]["dateTime"],"YYYY/MM/DD[T]HH:mm:ss"))).format("HH:mm:ss")
+			});
+		}; 
 	};
 
-
-
-
+console.log(timeDifferences)
 });
 
