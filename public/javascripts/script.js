3c984c5496d7f54637812248e9ec30cb4b1b1692
Suyeon Son <suyeon.son@icloud.com>
2016-01-16 20:18:09 +0900

    update google cal link & credit name

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,4 @@
-var getCalendar = "https://www.googleapis.com/calendar/v3/calendars/4cpkvhth0nvtedeo8t7tlfbupk@group.calendar.google.com/events?key=AIzaSyDFIPR7NpYdr5-2ykZqjoMsuT9EYW_zt_M";
+var getCalendar = "https://www.googleapis.com/calendar/v3/calendars/c80llbkpvtims9tsp6317el5ik@group.calendar.google.com/events?key=AIzaSyDFIPR7NpYdr5-2ykZqjoMsuT9EYW_zt_M";
 
 $(document).ready(function() {
 
